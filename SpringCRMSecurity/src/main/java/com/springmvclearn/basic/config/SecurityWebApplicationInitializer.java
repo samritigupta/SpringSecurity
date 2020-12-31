8bd90e6c8639e26d9a518cd0989e2d8d4edbd5e2
samritigupta <gupta.samriti1991@gmail.com>
2020-12-31 12:24:41 +0000

    CRM project with spring security

---
+++ SpringCRMSecurity/src/main/java/com/springmvclearn/basic/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.springmvclearn.basic.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ SpringCRMSecurity/src/main/java/com/springmvclearn/basic/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.springmvclearn.basic.service;
+
+import com.springmvclearn.basic.entity.Customer;
+
+import java.util.List;
+
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
